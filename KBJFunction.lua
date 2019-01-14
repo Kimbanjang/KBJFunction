f8ceffbf965e3128423875d74f6cbdbda00461bf
KYW <imkyw@outlook.com>
2019-01-14 09:19:08 +0900

    remove no use func

--- KBJFunction.lua
+++ KBJFunction.lua
@@ -12,78 +12,6 @@ hooksecurefunc("MovementSpeed_OnEnter", function(statFrame, unit)
 end)
 
 --------------------------------------------------------------------------------------------------------
--- FixOrderHallMap 1.0.3 by Ketho
--- https://mods.curse.com/addons/wow/265656-fixorderhallmap
---------------------------------------------------------------------------------------------------------
-local kbjFuncFindNeedFixMap = {
-	[23] = function() return select(4, GetMapInfo()) and 1007 end, -- Paladin, Sanctum of Light; Eastern Plaguelands
-	[1040] = function() return 1007 end, -- Priest, Netherlight Temple; Azeroth
-	[1044] = function() return 1007 end, -- Monk, Temple of Five Dawns; none
-	[1048] = function() return 1007 end, -- Druid, Emerald Dreamway; none
-	[1052] = function() return GetCurrentMapDungeonLevel() > 1 and 1007 end, -- Demon Hunter, Fel Hammer; Mardum
-	[1088] = function() return GetCurrentMapDungeonLevel() == 3 and 1033 end, -- Nighthold -> Suramar
-}
-
-local kbjFuncfixMap = WorldMapZoomOutButton_OnClick
-function WorldMapZoomOutButton_OnClick()
-	local id = kbjFuncFindNeedFixMap[GetCurrentMapAreaID()]
-	local out = id and id()
-	if out then
-		SetMapByID(out)
-	else
-		kbjFuncfixMap()
-	end
-end
-
---------------------------------------------------------------------------------------------------------
--- 2_Dealer 212.261 by 신_선 (modify : Kimbanjang)
--- http://wow.inven.co.kr/dataninfo/addonpds/detail.php?idx=7410
---------------------------------------------------------------------------------------------------------
--- config
-local gg = false	-- 길드 수리비 사용 한다( true ), 안한다( false )
--- /config
-
-local kbjFuncDealer = CreateFrame('Frame', nil, MerchantFrame)
-local kbjFuncDealer_onEvent = function(self, event)
-	if (event == 'MERCHANT_SHOW') then
-		for bag = 0, 4 do	-- 잡템 판매
-			for slot = 0, GetContainerNumSlots(bag) do
-				local S = GetContainerItemLink(bag, slot)
-				if S and string.find(S, "ff9d9d9d") then
-					UseContainerItem(bag, slot)
-				end
-			end
-		end
-
-		if CanMerchantRepair() then	-- 자동 수리
-			local co = GetRepairAllCost()
-			if (not co or co == 0) then	return
-			elseif gg and CanGuildBankRepair() then
-				local _, _, gI = GetGuildInfo("player")
-				self:RegisterEvent('UI_ERROR_MESSAGE')
-
-				if	(gI~=0 and GetGuildBankWithdrawMoney()<co) or GetGuildBankMoney()==0
-				or	(GetGuildBankMoney()<co and GetGuildBankMoney()>0) then
-					RepairAllItems()
-					print("|cFFFFCC00 수리비 : ", GetMoneyString(co).."  |c0000CC00(길드 수리비 부족)" )
-				else
-					RepairAllItems(1)
-					print("|cFFFFCC00 수리비 |c0000CC00(길드) |cFFFFCC00: ", GetMoneyString(co))
-				end
-				self:UnregisterEvent('UI_ERROR_MESSAGE')
-			elseif  GetMoney() < co then
-				print("|cFFFFCC00 수리비 부족")
-			else
-				RepairAllItems()
-				print("|cFFFFCC00 수리비 : ", GetMoneyString(co))
-			end
-		end
-	end
-end
-kbjFuncDealer:SetScript('OnEvent', kbjFuncDealer_onEvent)
-kbjFuncDealer:RegisterEvent('MERCHANT_SHOW')
-
---------------------------------------------------------------------------------------------------------
 -- Uganda Bags 1.31 by 우간다짱 (modify : Kimbanjang)
 -- http://wow.inven.co.kr/dataninfo/addonpds/detail.php?idx=5545
 --------------------------------------------------------------------------------------------------------
@@ -143,8 +71,8 @@ local kbjFuncHealPotMacroIcon = CreateFrame('Frame')
 kbjFuncHealPotMacroIcon:SetScript('OnEvent', function(self, event, ...)
 	SetMacroItem("!HP",GetItemCount("생명석") == 0 and "해안 치유 물약" or "생명석")
 end)
-kbjFuncHealPotMacroIcon:RegisterEvent('BAG_UPDATE')
 kbjFuncHealPotMacroIcon:RegisterEvent('PLAYER_LOGIN')
+kbjFuncHealPotMacroIcon:RegisterEvent('BAG_UPDATE')
 
 --------------------------------------------------------------------------------------------------------
 -- Move ZoneAbilityFrame
